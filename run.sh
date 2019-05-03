4da653ea58a1c083534f1eb361b325d1c4886a28
Alfred Neumayer <dev.beidl@gmail.com>
2019-05-03 14:34:19 +0200

    click: Fix startup issues in run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ export LD_LIBRARY_PATH=$PWD/usr/lib:$LD_LIBRARY_PATH
 export QT_QUICK_CONTROLS_MOBILE=true
 export QT_QUICK_CONTROLS_STYLE=Material
 
-$PWD/usr/bin/qmlcreator
+exec $PWD/usr/bin/qmlcreator \
+    --desktop_file_hint=${HOME}/.local/share/applications/${APP_ID}.desktop "$@"
