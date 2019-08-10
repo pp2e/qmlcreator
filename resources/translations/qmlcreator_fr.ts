8d3bb361f56dee45d63483777a70c03b754a3e01
Anne017 <anneonyme017@netcourrier.com>
2019-08-10 14:04:28 +0200

    French translation added

---
+++ resources/translations/qmlcreator_fr.ts
@@ -0,0 +1,317 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>AboutScreen</name>
+    <message>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+</context>
+<context>
+    <name>CApplicationWindow</name>
+    <message>
+        <source>New examples</source>
+        <translation>Nouveaux exemples</translation>
+    </message>
+    <message>
+        <source>We detected that you had recently updated QML Creator on your device.</source>
+        <translation>Nous avons détecté que vous avez récemment mis à jour QML Creator sur votre appareil.</translation>
+    </message>
+    <message>
+        <source>We are constantly working on QML Creator improvement, and we may have added some new sample projects in the current release.</source>
+        <translation>Nous travaillons sans relâche sur l'amélioration de QML Creator et il se peut que nous ayions ajouté de nouveaux exemples de projets dans la version actuelle.</translation>
+    </message>
+    <message>
+        <source>Press OK if you would like to get them now (notice that all the changes you have made in the Examples section will be removed)</source>
+        <translation>Appuyez sur Valider si vous souhaitez les obtenir maintenant (notez que toutes les modifications que vous avez faites dans la section Exemples seront supprimées)</translation>
+    </message>
+    <message>
+        <source>Alternatively, you can do it later by pressing the Restore examples button in the Examples screen.</source>
+        <translation>Sinon, vous pouvez le faire plus tard en appuyant sur le bouton Restaurer les exemples sur l'écran Exemples.</translation>
+    </message>
+</context>
+<context>
+    <name>CCodeArea</name>
+    <message>
+        <source>Undo</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Rétablir</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Coller</translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmationDialog</name>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>Valider</translation>
+    </message>
+</context>
+<context>
+    <name>EditorScreen</name>
+    <message>
+        <source>Select all</source>
+        <translation>Tout sélectionner</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Copier</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Couper</translation>
+    </message>
+    <message>
+        <source>Run</source>
+        <translation>Exécuter</translation>
+    </message>
+</context>
+<context>
+    <name>ExamplesScreen</name>
+    <message>
+        <source>Examples</source>
+        <translation>Exemples</translation>
+    </message>
+    <message>
+        <source>Restore the examples</source>
+        <translation>Restaurer les exemples</translation>
+    </message>
+    <message>
+        <source>Press OK to delete all the edits you have made in the Examples section.</source>
+        <translation>Appuyez sur Valider pour supprimer toutes les modifications que vous avez faites dans la section Exemples.</translation>
+    </message>
+    <message>
+        <source>Delete the example</source>
+        <translation>Supprimer l'exemple</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete &quot;%1&quot;?</source>
+        <translation>Voulez-vous vraiment supprimer « %1 » ?</translation>
+    </message>
+</context>
+<context>
+    <name>FilesScreen</name>
+    <message>
+        <source>New file</source>
+        <translation>Nouveau fichier</translation>
+    </message>
+    <message>
+        <source>Delete the file</source>
+        <translation>Supprimer le fichier</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete &quot;%1&quot;?</source>
+        <translation>Voulez-vous vraiment supprimer « %1 » ?</translation>
+    </message>
+</context>
+<context>
+    <name>FontSizeDialog</name>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>Valider</translation>
+    </message>
+</context>
+<context>
+    <name>IndentSizeDialog</name>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>Valider</translation>
+    </message>
+</context>
+<context>
+    <name>MainMenuScreen</name>
+    <message>
+        <source>PROJECTS</source>
+        <translation>PROJETS</translation>
+    </message>
+    <message>
+        <source>EXAMPLES</source>
+        <translation>EXEMPLES</translation>
+    </message>
+    <message>
+        <source>SETTINGS</source>
+        <translation>PARAMÈTRES</translation>
+    </message>
+    <message>
+        <source>MODULES</source>
+        <translation>MODULES</translation>
+    </message>
+    <message>
+        <source>ABOUT</source>
+        <translation>À PROPOS</translation>
+    </message>
+</context>
+<context>
+    <name>MessageDialog</name>
+    <message>
+        <source>OK</source>
+        <translation>Valider</translation>
+    </message>
+</context>
+<context>
+    <name>ModulesScreen</name>
+    <message>
+        <source>Modules</source>
+        <translation>Modules</translation>
+    </message>
+    <message>
+        <source>Available</source>
+        <translation>Disponible</translation>
+    </message>
+    <message>
+        <source>Not available</source>
+        <translation>Indisponible</translation>
+    </message>
+    <message>
+        <source>Undefined</source>
+        <translation>Indéfini</translation>
+    </message>
+</context>
+<context>
+    <name>NewFileDialog</name>
+    <message>
+        <source>File name</source>
+        <translation>Nom de fichier</translation>
+    </message>
+    <message>
+        <source>File type</source>
+        <translation>Type de fichier</translation>
+    </message>
+    <message>
+        <source>Note: the file name must start with a capital letter and cannot contain spaces (e.g., MyItem.qml)</source>
+        <translation>Note : le nom de fichier doit commencer par une lettre majuscule et ne peut pas contenir d'espaces (par exemple, MonObjet.qml)</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>Valider</translation>
+    </message>
+    <message>
+        <source>The file name cannot be left blank</source>
+        <translation>Le nom de fichier ne doit pas contenir d'espaces</translation>
+    </message>
+    <message>
+        <source>The file already exists</source>
+        <translation>Ce fichier existe déjà</translation>
+    </message>
+</context>
+<context>
+    <name>NewProjectDialog</name>
+    <message>
+        <source>Project name</source>
+        <translation>Nom du projet</translation>
+    </message>
+    <message>
+        <source>Note: the project name cannot contain spaces (e.g., My_Project)</source>
+        <translation>Note : le nom du projet ne doit pas contenir d'espaces (par exemple, Mon_Projet)</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>Valider</translation>
+    </message>
+    <message>
+        <source>The project name cannot be left blank</source>
+        <translation>Le nom du projet ne doit pas être laissé vide</translation>
+    </message>
+    <message>
+        <source>The project already exists</source>
+        <translation>Ce projet existe déjà</translation>
+    </message>
+</context>
+<context>
+    <name>PlaygroundScreen</name>
+    <message>
+        <source>Disable debugging</source>
+        <translation>Désactiver le débogage</translation>
+    </message>
+    <message>
+        <source>Enable debugging</source>
+        <translation>Activer le débogage</translation>
+    </message>
+</context>
+<context>
+    <name>ProjectsScreen</name>
+    <message>
+        <source>Projects</source>
+        <translation>Projets</translation>
+    </message>
+    <message>
+        <source>New project</source>
+        <translation>Nouveau projet</translation>
+    </message>
+    <message>
+        <source>Delete the project</source>
+        <translation>Supprimer le projet</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete &quot;%1&quot;?</source>
+        <translation>Voulez-vous vraiment supprimer « %1 » ?</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsScreen</name>
+    <message>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <source>Font family</source>
+        <translation>Famille de police</translation>
+    </message>
+    <message>
+        <source>Font size</source>
+        <translation>Taille de police</translation>
+    </message>
+    <message>
+        <source>Indent size</source>
+        <translation>Taille des indentations</translation>
+    </message>
+    <message>
+        <source>Palette</source>
+        <translation>Palette</translation>
+    </message>
+    <message>
+        <source>Editor font</source>
+        <translation>Éditeur de police</translation>
+    </message>
+    <message>
+        <source>Debugging</source>
+        <translation>Débogage</translation>
+    </message>
+    <message>
+        <source>Enabled</source>
+        <translatorcomment>В женском роде (отладка)</translatorcomment>
+        <translation>Activé</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translatorcomment>В женском роде (отладка)</translatorcomment>
+        <translation>Désactivé</translation>
+    </message>
+</context>
+</TS>
