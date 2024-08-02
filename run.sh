75ec86955fdb7c86ebb5e26900c2e34bbbedd455
New me! <enuromskiy@gmaii.com>
2024-08-02 19:59:52 +0300

    remove and simplifuy

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export LD_LIBRARY_PATH=$PWD/usr/lib:$LD_LIBRARY_PATH
-export QT_QUICK_CONTROLS_MOBILE=true
-export QT_QUICK_CONTROLS_STYLE=Material
-
-exec $PWD/usr/bin/qmlcreator \
-    --desktop_file_hint=${HOME}/.local/share/applications/${APP_ID}.desktop "$@"
